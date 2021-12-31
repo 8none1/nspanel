351680eab2e7192728e62c28193907879c2505e1
8none1 <will@whizzy.org>
2021-12-31 20:49:25 +0000

    Add PNG scaled images generated from SVGs Includes imagemagick script to regenerate

---
+++ assets/icons/meteocons-icons/SVG/processed/imagemagick_generate_bitmaps.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# This will use imagemagick to resize and recolour the SVGs to the required size for the NSPanel UI
+# It expects the images to have the lines already converted to pure black
+
+BACKGROUND_COL="#d8d7d2"
+mkdir -p 55x55_png
+mkdir -p 138x138_png
+
+for f in *.svg; do
+  OUTPUTFILE="55x55_png/55x55_${f%.*}.png";
+  convert $f -background "$BACKGROUND_COL" -fill "$BACKGROUND_COL" -flatten +opaque black -alpha remove -resize 55x55 -strip $OUTPUTFILE
+  OUTPUTFILE="138x138_png/138x138_${f%.*}.png";
+  convert $f -background "$BACKGROUND_COL" -fill "$BACKGROUND_COL" -flatten +opaque black -alpha remove -resize 138x138 -strip $OUTPUTFILE
+done
