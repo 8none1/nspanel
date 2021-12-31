ac055e6bc382e7bfdbefa5b0c96235bbdc1d7832
8none1 <will@whizzy.org>
2021-12-31 21:02:12 +0000

    Add 22x22 as well And Michael Fish

--- assets/icons/meteocons-icons/SVG/processed/imagemagick_generate_bitmaps.sh
+++ assets/icons/meteocons-icons/SVG/processed/imagemagick_generate_bitmaps.sh
@@ -3,10 +3,13 @@
 # It expects the images to have the lines already converted to pure black
 
 BACKGROUND_COL="#d8d7d2"
+mkdir -p 22x22_png
 mkdir -p 55x55_png
 mkdir -p 138x138_png
 
 for f in *.svg; do
+  OUTPUTFILE="22x22_png/22x22_${f%.*}.png";
+  convert $f -background "$BACKGROUND_COL" -fill "$BACKGROUND_COL" -flatten +opaque black -alpha remove -resize 22x22 -strip $OUTPUTFILE
   OUTPUTFILE="55x55_png/55x55_${f%.*}.png";
   convert $f -background "$BACKGROUND_COL" -fill "$BACKGROUND_COL" -flatten +opaque black -alpha remove -resize 55x55 -strip $OUTPUTFILE
   OUTPUTFILE="138x138_png/138x138_${f%.*}.png";
