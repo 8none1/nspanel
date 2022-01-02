352382017c2a413a868e4c333a1ba2ba1c6683a6
8none1 <will@whizzy.org>
2022-01-02 13:37:39 +0000

    Add start of Arduino sketch

--- backups/esp32/README.md
+++ backups/esp32/README.md
@@ -1,7 +1,7 @@
 This directory would contain a backup of the original nspanel ESP32 firmware.  It's a dump of the full 4MB flash and can be used to restore the ESP32 part of the NSPanel back to the factory state.
 Except of course it's littered with my wifi password in clear text, so I'm adding a git ignore to not upload it.  I imagine that it's not that hard to get a copy of the original clean FW.
 
-Actually, I'm uploaded an encrypted copy just in case I need it.  If you're desperate for a copy let me know.
+Actually, I'm uploading an encrypted copy just in case I need it.  If you're desperate for a copy let me know.
 
 It was backed up twice and hashed to make sure it's correct.  The procedure to download the flash is docuemnted here: https://cyberblogspot.com/how-to-save-and-restore-esp8266-and-esp32-firmware/
 Nice work that guy.
---
+++ webupload_tft_esp32.ino/webupload_tft_esp32.ino.ino
@@ -0,0 +1,86 @@
+#include "wificreds.h"
+#include <WiFi.h>
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+#include <ESP32Servo.h>
+
+unsigned long previousMillis = 0;
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(STASSID, STAPSK);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  // Port defaults to 3232
+  // ArduinoOTA.setPort(3232);
+
+  // Hostname defaults to esp3232-[MAC]
+  // ArduinoOTA.setHostname("myesp32");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+
+  ArduinoOTA.begin();
+
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // NS Panel Specifics
+  //pinMode(21, OUTPUT); // Buzzer. GPIO21 but which port do I put here?
+  ESP32PWM::allocateTimer(0);
+  ESP32PWM::allocateTimer(1);
+  ESP32PWM::allocateTimer(2);
+  ESP32PWM::allocateTimer(3);  
+  pinMode(4, OUTPUT);  // Screen reset. High = on?
+  digitalWrite(4, LOW); // Switch on screen, LOW = on.
+    
+}
+
+void loop() {
+  ArduinoOTA.handle();
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= 2500) {
+    previousMillis = millis();
+    //tone(21,4186, 100);
+  }
+  
+}
