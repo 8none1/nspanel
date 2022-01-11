f822cb73f7e7633781dff3e9e87d83b1693f33dc
8none1 <will@whizzy.org>
2022-01-11 17:18:57 +0000

    Chnange background colour to match more closely the paint colour. My house is not painted orange. This screen is a 565 colour device and represents colours differently.

--- assets/icons/weather-icons/inkscape_generate_bitmaps.sh
+++ assets/icons/weather-icons/inkscape_generate_bitmaps.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Inkscape is waaaaaay better at this than imagemagick.
 
-BACKGROUND_COL="#d8d7d2"
+#BACKGROUND_COL="#d8d7d2"
+BACKGROUND_COL="#ef9a5a"
 
 #mkdir -p 18x18_png
 #mkdir -p 22x22_png
@@ -12,12 +13,12 @@ mkdir -p 138x138_png
 for f in *.svg; do
   OUTPUTFILE="55x55_png/55x55_${f%.*}.png";
   #inkscape -w 55 -d 55 -b "#d8d7d2" -y 1 -e $OUTPUTFILE $f
-  inkscape -w 55 -y 1 -D -b "#d8d7d2" -y 1 -e $OUTPUTFILE $f
+  inkscape -w 55 -y 1 -D -b "$BACKGROUND_COL" -y 1 -e $OUTPUTFILE $f
 
   OUTPUTFILE="46x46_png/46x46_${f%.*}.png";
-  inkscape -w 46 -d 46 -b "#d8d7d2" -D -y 1 -e $OUTPUTFILE $f
+  inkscape -w 46 -d 46 -b "$BACKGROUND_COL" -D -y 1 -e $OUTPUTFILE $f
 
 
   OUTPUTFILE="138x138_png/138x138_${f%.*}.png";
-  inkscape -w 138 -d 138 -b "#d8d7d2" -D -y 1 -e $OUTPUTFILE $f
+  inkscape -w 138 -d 138 -b "$BACKGROUND_COL" -D -y 1 -e $OUTPUTFILE $f
 done
